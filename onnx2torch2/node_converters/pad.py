ef18a9d4125ea676efc410ad8d442afed383a45f
huayinl <hclover2003@gmail.com>
2024-10-24 13:28:02 -0400

    change comment

--- onnx2torch2/node_converters/pad.py
+++ onnx2torch2/node_converters/pad.py
@@ -123,7 +123,7 @@ def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:  # pylint: 
     pads_name = node.input_values[1]
     pads = []
     if pads_name in graph.initializers or pads_name in graph._node_output_values:  # pylint: disable=W0212
-        # try and get padding value frpads = Noneom node, or set as None
+        # try and get padding arg value
         try:
             pads = get_const_value(pads_name, graph).tolist()
         except KeyError as e:
